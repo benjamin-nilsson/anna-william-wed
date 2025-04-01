754c3747533ccada0b5103d2f7ea02f0f21dbb69
benjamin-nilsson <benjamin_nilsson@outlook.com>
2025-04-01 20:50:31 +0200

    Removed add item button.

--- src/components/wishlist/Wishlist.js
+++ src/components/wishlist/Wishlist.js
@@ -89,11 +89,6 @@ const Wishlist = () => {
       <div className="wishlist-desc"><p>Det viktigaste för oss är att få fira denna dag med er, men vi uppskattar gärna ett litet bidrag till vår framtid tillsammans.
          För detta ser vi gärna pengar, men nedan finns ocskå en lista på saker vi önskar oss.
          Man kan köpa dessa sjäv eller gå ihop några stycken, man kan markera och avmarkera närsom och man kan därmed återkomma till detta vid ett senare tillfälle.</p></div>
-      <div className="add-item-container">
-        <button className="add-item-btn" onClick={() => setShowForm(!showForm)}>
-          {showForm ? "Close" : "Add Item"}
-        </button>
-      </div>
 
       {showForm && (
         <form className="wishlist-form" onSubmit={handleAddItem}>
